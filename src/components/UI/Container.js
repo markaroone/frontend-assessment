add254b7e6e1a08ef6e19e9008116bf433bcee7f
Mark Christian Fajardo <cfajardo25@gmail.com>
2022-08-17 16:45:35 +0800

    fix: remove unused components

--- src/components/UI/Container.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Container = ({ elementType }) => {
-  let element = elementType?.toLowerCase() || 'section';
-
-  return <div>Container</div>;
-};
-
-export default Container;
